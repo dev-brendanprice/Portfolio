1d3f7af1c5162298170323029693a57c770c1bea
Brendan Price <56489848+dev-brendanprice@users.noreply.github.com>
2026-02-04 23:00:03 +0000

    Add profile segment style and more content

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,7 +1,28 @@
+import profilePicture from "../assets/profile-picture.png";
+import signature from "../assets/signature.svg";
+import linkedin from "../assets/linkedin.svg";
+import octo from "../assets/octo.svg";
+import cv from "../assets/cv.svg";
+
 export function Profile() {
   return (
     <div className="profile">
-      <div>Profile Image</div>
+
+      <div className="text-content">
+        <div className="profile-name">Brendan Price, 22</div>
+        <img src={profilePicture} alt="Profile Picture" className="profile-picture"></img>
+        <p>Applying a rigorously developed blend of software engineering, Microsoft administration, network engineering and architecture, technical support, Windows systems expertise, and continuous improvement of resilient, scalable digital environments with broad real-world impact.</p>
+      </div>
+
+      <div className="signature-links">
+        <img src={signature} alt="Photo of Signature" className="profile-signature"></img>
+        <div className="profile-links">
+          <img src={linkedin} alt="LinkedIn Link" className="link"></img>
+          <img src={octo} alt="GitHub Link" className="link"></img>
+          <img src={cv} alt="CV Link" className="link"></img>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,25 +1,25 @@
-import HeaderImage from "../assets/full-section-header-art.png";
+import headerImage from "../assets/full-section-header-art.png";
 import { Card } from "./card";
 
 export function Projects() {
   return (
     <div className="projects">
 
-      <img className="headerArt" src={HeaderImage} alt="Section Header Art" />
+      <img className="headerArt" src={headerImage} alt="Section Header Art" />
       
-      <p>Aspiring network engineer and technician looking to build the next generation of bleeding-edge infrastructure. First-Class Bachelor of Science (Hons) in Digital Technologies from Northampton University. Lor-em ipsum sola dat ipsum far reh so me tih doh. Lorem ipsum sola dat ipsum far reh so me tih doh. Lore-m ipsum sola dat ipsum far reh so me tih doh. Lorem ipsum sola dat ipsum far reh so me tih doh. Lorem ipsum sola dat ipsum far reh so me tih doh.</p>
+      <p>Aspiring technician and software-focused bachelor, looking to build the next generation of bleeding-edge infrastructure. I graduated with a First-Class Bachelor of Science (Hons) in Digital Technologies from Northampton University. My areas of expertise include Windows systems administration, Microsoft 365 and AD/DS administration, network engineering and architecture, software development and deployment, and technical support. I am an "OK" programmer.</p>
       
       <div className="cards">
 
         {/* first row :3 */}
-        <Card inlineStat="80%" content="time reduction in image deployment, as opposed to manually upgrading clients ad-hoc, entire groups can be imaged simultaneously and on-the-fly." />
-        <Card inlineStat="6000%" content="increase in user inflow from ~100 to 2000+ DAU, following a critical Esports exhibition where brand exposure was critical." />
-        <Card inlineStat="50%" content="faster ticket resolution times following a critical, and reimagined, foundational change in how tickets are triaged and handled." />
+        <Card inlineStat="80%" content="reduction in time through configured MDT/WDS task sequences, automated driver injection, and consistent deployment of Sysprep base images." />
+        <Card inlineStat="200%" content="increase in user inflow from ~100 to 2000+ DAU, following a critical Esports exhibition where brand exposure was critical." />
+        <Card inlineStat="100%" content="service availability for core infrastructure, with zero downtime during imaging, deployment and critical operational periods." />
 
         {/* second row :3 */}
-        <Card inlineStat="80%" content="time reduction in image deployment, as opposed to manually upgrading clients ad-hoc, entire groups can be imaged simultaneously and on-the-fly." />
-        <Card inlineStat="6000%" content="increase in user inflow from ~100 to 2000+ DAU, following a critical Esports exhibition where brand exposure was critical." />
-        <Card inlineStat="50%" content="faster ticket resolution times following a critical, and reimagined, foundational change in how tickets are triaged and handled." />
+        <Card inlineStat="99%" content="success rate on the deployment of updates, critical software, drivers, and patches across a lab environment of close to 100 devices." />
+        <Card inlineStat="~30%" content="reduction in repeat support tickets following consistent configuration baselines, and clearer documentation for troubleshooting." />
+        <Card inlineStat="40%" content="quicker first-time resolution of tickets through clearer fault isolation, structured escalation and troubleshooting during frontline IT support incidents." />
       
       </div>
     </div>
